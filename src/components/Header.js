0ff0f070128c14606c7b37cba92203adb6c25c2c
FaziletKosure <faziletkosure1@gmail.com>
2021-03-22 15:09:52 +0100

    added typed effect

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,18 @@
 import React from "react";
+import Typed from "react-typed";
 
 const Header = () => {
   return (
     <div className="header-wraper">
       <div className="main-info">
         <h1>web development and websites promotions</h1>
+        <Typed
+          className="typed-text"
+          strings={["Web Development", "Web Design", "Search for brands"]}
+          typeSpeed={40}
+          backSpeed={50}
+          loop
+        />
       </div>
     </div>
   );
