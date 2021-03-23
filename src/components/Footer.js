f5f0d35cc8100a482afde8832005e67b5e724187
FaziletKosure <faziletkosure1@gmail.com>
2021-03-23 15:37:26 +0100

    created footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,95 @@
+import React from "react";
+import {
+  FacebookShareButton,
+  FacebookIcon,
+  TwitterShareButton,
+  TwitterIcon,
+  RedditShareButton,
+  RedditIcon,
+  LinkedinShareButton,
+  LinkedinIcon,
+} from "react-share";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-4 col-md-6 col-sm-6">
+            <div className="d-flex">
+              <p>city Antwerpen</p>
+            </div>
+            <div className="d-flex">
+              <a href="tel:555-555-555">+32(493)555-5555</a>
+            </div>
+            <div className="d-flex">
+              <p>faziletkosure1@gmail.com</p>
+            </div>
+          </div>
+          <div className="col-lg-3 col-md-2 col-sm-6">
+            <div className="row">
+              <div className="col">
+                <a className="footer-nav">Home</a>
+                <br />
+                <a className="footer-nav">About me</a>
+                <br />
+                <a className="footer-nav">Services</a>
+              </div>
+              <div className="col">
+                <a className="footer-nav">Experience</a>
+                <br />
+                <a className="footer-nav">Portfolio</a>
+                <br />
+                <a className="footer-nav">Contacts</a>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-5 col-md-5 col-sm-6 align-items-center">
+            <div className="d-flex justify-content-center">
+              <FacebookShareButton
+                url={"https://faziletkosure1.medium.com/"}
+                quote={"FullStack Developer"}
+                hashtag="#javascript"
+              >
+                <FacebookIcon className="mx-3" size={36} />
+              </FacebookShareButton>
+              <TwitterShareButton
+                url={
+                  "https://www.youtube.com/watch?v=cU8Bt5uX2hw&ab_channel=faziletkosure"
+                }
+                quote={"FullStack Developer"}
+                hashtag="#javascript"
+              >
+                <TwitterIcon className="mx-3" size={36} />
+              </TwitterShareButton>
+              <RedditShareButton
+                url={
+                  "https://www.linkedin.com/in/faziletkosure/?originalSubdomain=be"
+                }
+                quote={"FullStack Developer"}
+                hashtag="#javascript"
+              >
+                <RedditIcon className="mx-3" size={36} />
+              </RedditShareButton>
+              <LinkedinShareButton
+                url={
+                  "https://www.linkedin.com/in/faziletkosure/?originalSubdomain=be"
+                }
+                quote={"FullStack Developer"}
+                hashtag="#javascript"
+              >
+                <LinkedinIcon className="mx-3" size={36} />
+              </LinkedinShareButton>
+            </div>
+            <p className="pt-3 text-center">
+              Copyright&copy;
+              {new Date().getFullYear()}&nbsp;Coder Fazi | All Rights Reserved
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
