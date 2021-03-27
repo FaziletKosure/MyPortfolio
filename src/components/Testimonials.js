a92ebd735864cbe792292abdda9331193f371d5c
FaziletKosure <faziletkosure1@gmail.com>
2021-03-27 23:22:25 +0100

    created testimonials

---
+++ src/components/Testimonials.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Testimonials = () => {
+  return (
+    <div className="testimonials">
+      <h1>my happy clients</h1>
+    </div>
+  );
+};
+
+export default Testimonials;
